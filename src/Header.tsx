2d43a9a7677136df598be29c274be781799cac6f
kkznch <kkznch710md@gmail.com>
2021-08-01 16:06:24 +0900

    2-3 App コンポーネントを分割し、コンポーネントの分割に慣れる

---
+++ src/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import * as color from './color'
+import { CardFilter } from './CardFilter'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  background-color: ${color.Navy};
+`
+
+const Logo = styled.div`
+  color: ${color.Silver};
+  font-size: 16px;
+  font-weight: bold;
+`
+
+export const Header = ({ className }: { className?: string }) => (
+  <Container className={className}>
+    <Logo>Kanban board</Logo>
+
+    <CardFilter />
+  </Container>
+)
